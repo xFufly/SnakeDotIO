8ec47ad21f495fadabca0de801570de67cc7c33a
DIDELOT Tim <tdidelot@iutbayonne.univ-pau.fr>
2025-09-22 16:15:51 +0200

    [+] Ranking btn in index

--- php/index.php
+++ php/index.php
@@ -5,3 +5,15 @@ if (!isset($_SESSION["user_id"])) {
     exit();
 }
 ?>
+<!DOCTYPE html>
+<html lang="en">
+    <body>
+        <header>
+            <h1>Welcome to Snake.io, <?php echo htmlspecialchars($_SESSION["user_id"]); ?>!</h1>
+            <nav>
+                <ul>
+                    <li><a href="ranking.php">Ranking</a></li>
+                </ul>
+            </nav>
+    </body>
+</html>
