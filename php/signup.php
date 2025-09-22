89f587f7c971a4261259a27bd7fe45e3f461eb92
DIDELOT Tim <tdidelot@iutbayonne.univ-pau.fr>
2025-09-22 14:41:38 +0200

    [+] Correction, email in form

--- php/signup.php
+++ php/signup.php
@@ -1,6 +1,6 @@
 <?php
 
-if ($_POST["username"] && $_POST["password"]) {
+if ($_POST["username"] && $_POST["password"] && $_POST["email"]) {
     // Database configuration
     $servername = "localhost";
     $username = "admin";
@@ -13,9 +13,10 @@ if ($_POST["username"] && $_POST["password"]) {
     if ($conn->connect_error) {
         die("Connection failed: " . $conn->connect_error);
     }
+    $email = $conn->real_escape_string($_POST["email"]);
     $user = $_POST["username"];
     $pass = password_hash($_POST["password"], PASSWORD_BCRYPT);
-    $sql = "INSERT INTO users (username, password) VALUES ('$user', '$pass')";
+    $sql = "INSERT INTO users (username, password, email) VALUES ('$user', '$pass', '$email')";
     if ($conn->query($sql) === TRUE) {
         echo "New record created successfully";
     } else {
@@ -38,6 +39,7 @@ if ($_POST["username"] && $_POST["password"]) {
 <body>
     <form action="signup.php" method="POST" class="form-container">
         <h2>Sign Up</h2>
+        <input type="email" name="email" placeholder="Email" required>
         <input type="text" name="username" placeholder="Username" required>
         <input type="password" name="password" placeholder="Password" required>
         <button type="submit">Sign Up</button>
